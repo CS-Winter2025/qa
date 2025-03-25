e62479a8e492b42dc073cfeb4221c25557996c66
jimmytsang42 <jimmytsang42@gmail.com>
2025-03-25 14:54:33 -0700

    Initial push of Signup Login functionality
    Created a user model/controller in order to implement basic signup/login

---
+++ AppContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCSampleApp.Models;
+
+namespace MVCSampleApp
+{
+    public class AppContext : DbContext
+    {
+        public AppContext(DbContextOptions<AppContext> options) : base(options) { }
+        public AppContext() { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+        public DbSet<FullAddress> FullAddresses { get; set; }
+        public DbSet<FullName> FullNames { get; set; }
+        public DbSet<Person> People { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+
+}
